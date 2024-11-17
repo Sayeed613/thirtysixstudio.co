a92a31b46b73e1fba69162f58d542debe6d13605
Sayeed613 <sayeedahmed90082@gmail.com>
2024-11-17 20:18:35 +0530

    add data

--- src/Canvas.jsx
+++ src/Canvas.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from "react";
-import canvasImages from "./canvasimages.js";
+import canvasImages from "./assets/index";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 
